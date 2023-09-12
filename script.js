6ef4ec0d988d0eea0e308bf2863c427d3d2f9ea4
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-09-12 15:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-
-    if (!str) {
+	    if (!str) {
         return null;
     }
 
@@ -18,5 +17,8 @@ function firstNonRepeatedChar(str) {
         const char = str[i];
         if (charCount[char.charCodeAt(0) - 'a'.charCodeAt(0)] === 1) {
             return char;
-        }
-    }
+        }
+    }
+}
+const input = prompt("Enter a string");
+alert(firstNonRepeatedChar(input)); 
